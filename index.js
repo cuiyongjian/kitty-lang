3956e8af06b1a94b7bb8bac633b7e61cab60b79e
sheldon <websheldoncui@gmail.com>
2017-07-27 01:30:04 +0800

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @file 语言增强模块
+ * @author: cuiyongjian(cuiyongjian@outlook.com)
+ */
+
+
+module.exports = {
+    inherits: require('./lib/inherits'),
+    bind: require('./lib/bind'),
+    extend: require('./lib/extend'),
+    create: require('./lib/create'),
+    curry: require('./lib/curry'),
+	reduce: require('./lib/reduce'),
+    type: require('./lib/type')
+};
